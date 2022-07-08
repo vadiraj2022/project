0deee8487d8374e1a9444c21f3606662c19d2b49
root <root@ip-172-31-18-117.ec2.internal>
2022-07-08 05:09:08 +0000

    removed

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.7.3-alpine3.9
-RUN mkdir /myapp
-WORKDIR /myapp
-COPY app.py /myapp
-COPY requirements.txt /myapp
-RUN pip install -r requirements.txt
-ENTRYPOINT [ "python", "-u", "./app.py" ]
-
--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/hello/<name>')
-def hello_name(name):
-   return 'hello %s!' % name
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9095)
-
